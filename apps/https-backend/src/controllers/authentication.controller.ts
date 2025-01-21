757cd56f005b31b6d250a7e4646e2f605f5d6d57
[anudeep avula] <[anudeepavula009@gmail.com]>
2025-01-21 21:29:12 +0530

    integrate code folder structure

---
+++ apps/https-backend/src/controllers/authentication.controller.ts
@@ -0,0 +1,21 @@
+import { Jwt } from "jsonwebtoken";
+import { Request,Response } from "express";
+
+const signUp = (req : Request, res : Response) => {
+    try {
+        const { email, password } = req.body;
+
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+const signIn = (req : Request, res : Response) => {
+    try {
+        const { email, password } = req.body;
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+export { signUp, signIn }
--- apps/https-backend/src/server.ts
+++ apps/https-backend/src/server.ts
@@ -1,5 +1,14 @@
 import express from "express";
+import dotenv from "dotenv"
+import authRoutes from "./routes/authenticatioin.routes";
 
 const app = express();
+dotenv.config();
 
-app.listen(4006);
+const PORT = process.env.PORT || 3002
+
+app.use("api/auth",authRoutes);
+
+app.listen(PORT,() => {
+    console.log("http-backend in running");
+})
