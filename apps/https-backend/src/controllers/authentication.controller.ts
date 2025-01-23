8530d7ad0515e824e1cb36ce9d1fe55049c30a46
[anudeep avula] <[anudeepavula009@gmail.com]>
2025-01-23 12:06:51 +0530

    fix minor project strucuture bugs

--- apps/https-backend/src/controllers/authentication.controller.ts
+++ apps/https-backend/src/controllers/authentication.controller.ts
@@ -1,10 +1,10 @@
-import { Jwt } from "jsonwebtoken";
+import { prismaClient } from "@repo/db";
 import { Request,Response } from "express";
 
 const signUp = (req : Request, res : Response) => {
     try {
         const { email, password } = req.body;
-
+        
     } catch (error) {
         console.error(error);
     }
