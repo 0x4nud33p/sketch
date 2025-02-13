10ebade32c5bc9ad5e2b03ce16e4d82458c59093
[anudeep avula] <[anudeepavula009@gmail.com]>
2025-02-13 16:00:29 +0530

    cleaned used dependencies

--- apps/web/app/canvas/page.tsx
+++ apps/web/app/canvas/page.tsx
@@ -3,7 +3,9 @@
 const page = () => {
   return (
     <>
-      
+      <div className="bg-[#18181b] min-h-screen">
+        <h1>canvas</h1>
+      </div>
     </>
   )
 }
--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,9 +1,12 @@
-
+import { redirect } from "next/navigation";
 
 export default function Home() {
+  redirect("/canvas")
   return (
-    <div>
-      home page
+    <div className="bg-[#18181b]">
+      <h1 className="text-red-400 text-bold ">
+      Redirecting to canvas
+    </h1>
     </div>
   );
 }
